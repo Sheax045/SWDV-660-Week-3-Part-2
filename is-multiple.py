345b2872e4f8bbd633e40c8f7749c8ed065b81ad
Sheax045 <mkrbsb@yahoo.com>
2019-05-24 22:15:16 -0500

    Create is-multiple.py

---
+++ is-multiple.py
@@ -0,0 +1,12 @@
+n = int(input('Give me a number: '))
+m = int(input('Give me a second number: '))
+
+def is_multiple(n, m):
+    
+    if n % m == 0:
+        print( 'True,', n, 'is a multiple of', m)
+    
+    else:
+        print('False,', n, 'is not a multiple', m)
+    
+is_multiple(n, m)
